9872b050a229fd373f6839f23e2845e5b716f953
the Phoeron <thephoeron@me.com>
2014-06-28 11:51:31 -0400

    Update hash-table generation to use string-based keys to minimize potential symbol conflicts

--- maps.lisp
+++ maps.lisp
@@ -10,21 +10,20 @@
 
 (let ((cuneiform-plist #.(with-open-file (f (merge-pathnames "cuneiform-plist.lisp-expr" (or *compile-file-pathname* *load-truename*)))
                            (read f))))
-  (loop for (key . value) in cuneiform-plist
-        do (setf (gethash key *cuneiform-signs*) value)))
+  (loop for (sign . plist) in cuneiform-plist
+        do (setf (gethash sign *cuneiform-signs*) plist)))
 
 (defvar *sign-readings* (make-hash-table :test 'equalp :size 10000))
 
 (let ((sign-readings-plist #.(with-open-file (f (merge-pathnames "sign-readings-plist.lisp-expr" (or *compile-file-pathname* *load-truename*)))
                                (read f))))
-  (loop for (key . value) in sign-readings-plist
-        do (setf (gethash key *sign-readings*) value)))
+  (loop for (reading . plist) in sign-readings-plist
+        do (setf (gethash reading *sign-readings*) plist)))
 
 (defun get-sign-for-reading (sign-reading)
   "Return the Unicode cuneiform character for a given symbol, sign-reading."
   (let* ((r-plist (gethash sign-reading *sign-readings*))
-         (sign (getf r-plist :sign))
-         (c-plist (gethash sign *cuneiform-signs*)))
-    (getf c-plist :char)))
+         (signlist (getf r-plist :signs)))
+    (loop for sign in signlist collect (getf (gethash sign *cuneiform-signs*) :char))))
 
 ;; EOF
