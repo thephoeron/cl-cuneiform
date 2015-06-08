e78a96ed747e8b1c4b4f1ffa79fb7ceed5928db6
the Phoeron <thephoeron@users.noreply.github.com>
2015-06-08 01:32:55 -0400

    Update t/cl-cuneiform.lisp
    add sanity check test

--- t/cl-cuneiform.lisp
+++ t/cl-cuneiform.lisp
@@ -7,4 +7,15 @@
 
 (in-package :cl-cuneiform-test)
 
+;; NOTE: To run this test file, execute `(asdf:test-system :cl-cuneiform)' in your Lisp.
+
+(plan 1)
+
+(deftest sanity-check
+  (is (+ 1 1)
+      2
+      "Sane Lisp system."))
+
+(run-test-all)
+
 ;; EOF
